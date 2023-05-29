cd0d5fef568b8d83291ef154f18bc564d18ccb16
Cesar <CesarCordova.developer@gmail.com>
2023-05-28 20:25:56 -0700

    correcion de variables2 en la pag de serviciorapido

--- src/views/ServicioRapido.jsx
+++ src/views/ServicioRapido.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, React } from "react";
+import { useState, useEffect, React } from 'react'
 import {
   CCardHeader,
   CCardBody,
@@ -7,124 +7,115 @@ import {
   CContainer,
   CSmartTable,
   CRow,
-  CCol,
-} from "@coreui/react-pro";
-import { getProducts } from "@api/productos.api";
+  CCol
+} from '@coreui/react-pro'
+import { getProducts } from '@api/productos.api'
 // esto se va a borrar se usara los datos de la api
 const products = [
-  { id: 1, name: "Pan", precio: 1.5, quantity: 0, total: 0 },
-  { id: 2, name: "Torta", precio: 3.5, quantity: 0, total: 0 },
-  { id: 3, name: "Croissant", precio: 1.0, quantity: 0, total: 0 },
-];
-
-export default function ServicioRapido() {
+  { idproducto: 1, descripcion: 'Pan', precio: 1.5 },
+  { idproducto: 2, descripcion: 'Torta', precio: 3.5 },
+  { idproducto: 3, descripcion: 'Croissant', precio: 1.0 }
+]
+console.log(products)
+export default function ServicioRapido () {
   // estos son los estados o hooks
-  const [loading, setLoading] = useState();
-  const [cart, setCart] = useState([]);
-  const [items, setItems] = useState(products);
-  const [totalMax, setTotal] = useState(0);
+  const [loading, setLoading] = useState()
+  const [cart, setCart] = useState([])
+  const [items, setItems] = useState(products)
+  const [totalMax, setTotal] = useState(0)
 
   useEffect(() => {
     getProducts().then((res) => {
-      setItems(res.data);
-    });
-  }, []);
+      setItems(res.data)
+    })
+  }, [])
 
   // tabla de carrito de compras
   const columns = [
-    { key: "descripcion", label: "Producto" },
-    { key: "precio", label: "Precio" },
-    { key: "quantity", label: "Cantidad" },
-    { key: "total", label: "Total" },
+    { key: 'descripcion', label: 'Producto' },
+    { key: 'precio', label: 'Precio' },
+    { key: 'quantity', label: 'Cantidad' },
+    { key: 'total', label: 'Total' },
     {
-      key: "actions",
-      label: "Actions",
-      _style: { width: "10%" },
+      key: 'actions',
+      label: 'Actions',
+      _style: { width: '10%' },
       // sorter es para que no se pueda ordenar por esta columna
       sorter: false,
       // filter es para que no se pueda filtrar por esta columna
-      filter: false,
-    },
-  ];
+      filter: false
+    }
+  ]
 
   // tabla de productos disponibles
   const columns2 = [
-    { key: "descripcion", label: "Producto" },
-    { key: "precio", label: "Precio" },
+    { key: 'descripcion', label: 'Producto' },
+    { key: 'precio', label: 'Precio' },
     {
-      key: "actions",
-      label: "Actions",
-      _style: { width: "10%" },
+      key: 'actions',
+      label: 'Actions',
+      _style: { width: '10%' },
       sorter: false,
-      filter: false,
-    },
-  ];
+      filter: false
+    }
+  ]
 
   // funcion para cambiar la cantidad de productos
   const handleQuantityChange = (item, value) => {
     // esta funcion mapea los items y devuelve todos los items menos el que se quiere cambiar
     const updatedItems = cart.map((i) =>
-      i.id === item.id
-        ? // si el id del item es igual al id del item que se quiere cambiar se devuelve el item con la cantidad cambiada
+      i.idproducto === item.idproducto
+        ? // si el idproducto del item es igual al idproducto del item que se quiere cambiar se devuelve el item con la cantidad cambiada
           // el spread operator (...) es para que se devuelva el item con todas sus propiedades y solo se cambie la cantidad
           { ...i, quantity: parseInt(value), total: parseInt(value) * i.precio }
         : i
-    );
-    setCart(updatedItems);
-    setTotal(calculateTotal(updatedItems));
-  };
+    )
+
+    setCart(updatedItems)
+    setTotal(calculateTotal(updatedItems))
+    console.log(cart)
+  }
 
   // funcion para eliminar un producto
   const handleRemoveItem = (item) => {
     // esta funcion filtra los items y devuelve todos los items que no sean el que se quiere eliminar
-    const updatedItems = cart.filter((i) => i.id !== item.id);
-    setCart(updatedItems);
-    setTotal(calculateTotal(updatedItems));
-  };
+    const updatedItems = cart.filter((i) => i.idproducto !== item.idproducto)
+    setCart(updatedItems)
+    setTotal(calculateTotal(updatedItems))
+  }
 
   // funcion para calcular el total
   const calculateTotal = (items) => {
     // el metodo reduce sirve para sumar los valores de un array y el 0 es el valor inicial
-    return items.reduce((total, item) => total + item.total, 0);
-  };
+    return items.reduce((total, item) => total + item.total, 0)
+  }
 
   // funcion para agregar un producto al carrito
   const handleAddToCart = (item) => {
     // si el item ya esta en el carrito se actualiza la cantidad y el total del item en el carrito
-    if (cart.find((i) => i.id === item.id)) {
+    if (cart.find((i) => i.idproducto === item.idproducto)) {
       const updatedCart = cart.map((i) =>
-        i.id === item.id
+        i.idproducto === item.idproducto
           ? {
               ...i,
               quantity: i.quantity + 1,
-              total: (i.quantity + 1) * i.precio,
+              total: (i.quantity + 1) * i.precio
             }
           : i
-      );
-      return setCart(updatedCart), setTotal(totalMax + item.precio);
+      )
+      console.log(updatedCart)
+      return setCart(updatedCart), setTotal(totalMax + item.precio)
     }
     // si el item no esta en el carrito se agrega al carrito con cantidad 1 y total igual al precio
     const cartItem = {
       ...item,
       quantity: 1,
-      total: item.precio,
-    };
+      total: item.precio
+    }
     // se agrega el item al carrito y se suma el total set cart es para agregar el item al carrito
-    setCart([...cart, cartItem]);
-    setTotal(totalMax + item.precio);
-  };
-
-  // // funcion para traer los datos de la api
-  // const getUsers = useEffect(() => {
-  // 	setLoading(true);
-  // 	fetch('https://rickandmortyapi.com/api/character')
-  // 		.then((response) => response.json())
-  // 		.then((result) => {
-  // 			console.log(result.results);
-  // 			setUsers(result.results);
-  // 			setLoading(false);
-  // 		});
-  // }, []);
+    setCart([...cart, cartItem])
+    setTotal(totalMax + item.precio)
+  }
 
   return (
     <CContainer>
@@ -162,7 +153,7 @@ export default function ServicioRapido() {
                         Eliminar
                       </CButton>
                     </>
-                  ),
+                  )
                 }}
               ></CSmartTable>
               <hr />
@@ -200,7 +191,7 @@ export default function ServicioRapido() {
                         Agregar
                       </CButton>
                     </>
-                  ),
+                  )
                 }}
               />
             </CCardBody>
@@ -208,5 +199,5 @@ export default function ServicioRapido() {
         </CCol>
       </CRow>
     </CContainer>
-  );
+  )
 }
